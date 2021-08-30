5fcaf9dbe8a3ed830d5a131ad58af7b610a50377
Clesio <clesio.nascto@gmail.com>
2021-08-30 12:10:00 -0300

    Terminamos a parte de promises e encerramos a apostila 03

--- index.js
+++ index.js
@@ -1,5 +1,113 @@
+function fatorial (n){
+    if( n < 0) return Promise.reject ("Valor não pode ser negativo")
+    let res = 1
+    for (let i = 2; i <=n; i++)
+    res *= i
+    return Promise.resolve(res)
+}
+
+function chamadaComAssyncAwait () {
+    try {
+        const f1 = fatorial (5)
+        console.log(f1)
+        const f2 = await fatorial (-1)
+        console.log(f2)
+
+    }catch (e)[
+        console.log("Deu erro (mostrando com o try/catch" + e)
+    ]
+}
+
+chamadaComAssyncAwait()
 
 
+
+
+
+
+
+// function chamadaComThenCatch(){
+//     fatorial(5)
+//         .then (res => console.log ("Deu certo: " + res))
+//         .catch (erro => console.log ("Erro " + erro))
+
+//     fatorial (-1)
+//         .then (res => console.log ("Deu certo: " + res))    
+// }
+
+
+
+
+
+//  async function hello (nome){
+//     return `Olá, ${nome}`
+// }
+
+// const p = helo ('joão')
+
+// p.then (texto => console.log(texto))
+
+
+
+
+
+
+
+// function calculoRapidinho (numero){
+//     if (numero + 0){
+//     return Promise.resolve(numero * (numero + 1)) / 2);
+//     }
+//     return Promise.reject("Somente valores positivos, por favor")
+
+// }
+
+// calculoRapidinho (100).then((res) => { 
+// console.log ("res: " + res)})
+// .catch(erro => {
+//     console.log ("Erro: " + erro)
+// })
+
+// calculoRapidinho(-1).then ((res) => {
+//     console.log("Resultado obtido: " + res)
+// })
+// .catch (erro => {
+//     console.log ("Erro: " + erro)
+// }) 
+
+// console.log ("Encerrando o scrip principal")
+
+
+
+
+
+
+// function calculoDemorado (numero){
+//     return new Promise (function (resolve, reject){
+//         let res = 0 
+//         for (let i = 1; i <= numero; i++)
+//             res += i
+//             resolve(res)
+//     })
+// }
+
+// calculoDemorado(1000).then ((res) => {
+//     console.log("Resultado " + res)
+// })
+
+// console.log("Encerrando a execução do script principal")
+
+
+
+
+// function calculoDemorado (numero){
+//     let res = 0
+//     for(let i = 1; i <= numero; i++)
+//         res += i
+//         return res
+// }
+
+// console.log (`Soma(10): $(calculoDemorado(10))`)
+// console.log("Encerrando script principal...")
 
 
 
